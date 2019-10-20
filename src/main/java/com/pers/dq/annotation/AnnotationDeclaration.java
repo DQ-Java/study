3183797ba3e26cacdb7272d3d83139ab7d0ad454
Java-DQ <Qhl02103978>
2019-10-20 14:33:13 +0800

    注解类型声明

---
+++ src/main/java/com/pers/dq/annotation/AnnotationDeclaration.java
@@ -0,0 +1,20 @@
+package com.pers.dq.annotation;
+
+/**
+ * @author DQ
+ * @Description
+ * @create 2019-10-20 14:30
+ */
+public class AnnotationDeclaration {
+
+    //@符号与interface关键字无分隔符，常用风格
+    @interface Test{
+
+    }
+
+    //@符号与interface关键字用空格做分隔
+    @ interface Test1{
+
+    }
+
+}
